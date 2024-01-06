9a0ac38cf278515ce07d433d4bc8eb0cd26752fc
Corey Anderson <48457668+Aqueous20@users.noreply.github.com>
2024-01-05 22:14:13 -0500

    6th commit
    I got the hang of tailwind so the coding should be completed by tomorrow

--- velocity/src/App.js
+++ velocity/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="bg-orange-#f97316">
-      <h1 className="text-6xl text-center text-blue-400">Hello world</h1>
+      <h1 className="text-6xl text-center">Hello world</h1>
     </div>
   );
 }
