1d41073b98f91d90a9539bc704d4f6b656645fda
Corey Anderson <48457668+Aqueous20@users.noreply.github.com>
2024-01-06 22:21:44 -0500

    finished setting up css using tailwind added about component
    not much done today but I at least keeping my word of uploading every day or every 2 day something being done

--- velocity/src/App.js
+++ velocity/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="bg-orange-#f97316">
-      <h1 className="text-6xl text-center">Hello world</h1>
+    <div>
+      <h1>Hello world</h1>
     </div>
   );
 }
