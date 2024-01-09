908636f7b1bc906b3b7e359e0821ea529fe1d3b2
Corey Anderson <48457668+Aqueous20@users.noreply.github.com>
2024-01-08 22:38:04 -0500

    styled the header i feel it isnt dynamic enough may change at a later date
    added some stuff almost completed this project

--- velocity/src/App.js
+++ velocity/src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 //Components imported
-import Header from "./components/Header.js"
-import Hero from "./components/Hero.js"
-
-
+import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
+import About from "./components/About.js";
+import Cars from "./components/Cars.js";
+import Why from "./components/Why.js";
+import Testimonials from "./components/Testimonials.js";
+import Cta from "./components/Cta.js";
+import Footer from "./components/Footer.js";
+import BackToTopBtn from "./components/BackToTopBtn.js";
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="max-w-[1928px] bg-white mx-auto relative overflow-hidden">
+      <Header></Header>
+      <Hero></Hero>
+      <Cars></Cars>
+      <About></About>
+      <Why></Why>
+      <Testimonials></Testimonials>
+      <Cta></Cta>
+      <Footer></Footer>
+      <BackToTopBtn></BackToTopBtn>
+
+      <div className="h-[4000px]"></div>
     </div>
   );
 }
--- velocity/src/components/Header.js
+++ velocity/src/components/Header.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { useContext, useState, useEffect } from "react";
+//import Image from "next/image";
+import { Link } from "react-scroll";
+import SearchMobile from "./SearchMobile";
+import { useMediaQuery } from "react-responsive";
+import logo from "../assets/icons/logo.svg";
 
 function Header() {
-  return <div>Header</div>;
+  const [header, setHeader] = useState(false);
+  const [nav, setNav] = useState(false);
+
+  const desktopMode = useMediaQuery({
+    query: "(min-width: 1300px)",
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 40) {
+        setHeader(true);
+      } else {
+        setHeader(false);
+      }
+    };
+
+    //Add Event Listener
+    window.addEventListener("scroll", handleScroll);
+
+    //Remove Event Listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  console.log(header);
+
+  return (
+    <header
+      className={`${
+        header ? "bg-white shadow-md py-2" : "bg-transparent shadow-none py-4"
+      } fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}
+    >
+      <div className="xl:container mx-auto flex-col xl:flex-row xl:items-center xl:justify-between">
+        <div>
+          {/* logo */}
+          <Link>
+            <div className="w-[130px] h-fit">
+              <img src={logo} alt="binge sake"></img>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
