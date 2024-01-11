051f2128036b2dff736be462c16922e8a5f19b3a
Corey Anderson <48457668+Aqueous20@users.noreply.github.com>
2024-01-10 20:30:52 -0500

    updated Header again
    Added a few tweeks for responsiveness

--- velocity/src/components/Header.js
+++ velocity/src/components/Header.js
@@ -5,8 +5,7 @@ import { Link } from "react-scroll";
 import SearchMobile from "./SearchMobile";
 import { useMediaQuery } from "react-responsive";
 import logo from "../assets/icons/logo.svg";
-import {BiMenuAltRight, Bix} from 'react-icons/bi'
-
+import { BiMenuAltRight, BiX } from "react-icons/bi";
 
 function Header() {
   const [header, setHeader] = useState(false);
@@ -34,15 +33,13 @@ function Header() {
     };
   }, []);
 
-  
-
   return (
     <header
       className={`${
         header ? "bg-white shadow-md py-2" : "bg-transparent shadow-none py-4"
       } fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}
     >
-      <div className="xl:container mx-auto flex-col xl:flex-row xl:items-center xl:justify-between">
+      <div className="xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
         <div className="flex justify-between items-center px-4">
           {/* logo */}
           <Link
@@ -58,8 +55,90 @@ function Header() {
 
           {/* nav open menu */}
 
-          <div>menu</div>
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer xl:hidden"
+          >
+            {nav ? (
+              <BiX className="text-4xl" />
+            ) : (
+              <BiMenuAltRight className="text-4xl" />
+            )}
+          </div>
         </div>
+
+        {/* Nav */}
+
+        <nav
+          className={`${
+            nav ? "max-h-max py-8 px-4 xl:py-0 xl:px-0" : "max-h-0 xl:max-h-max"
+          } flex flex-col w-full bg-white gap-y-6 overflow-hidden font-bold xl:font-medium xl:flex-row xl:w-max`}
+        >
+          <Link
+            className="cursor-pointer"
+            to="home"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Home
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="cars"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Cars
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="about"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            About
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="why"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Why us
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="testimonials"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Testimonials
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="contact"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Contact
+          </Link>
+          <Link
+            className="xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto"
+            to="/"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            See all cars
+          </Link>
+          <SearchMobile></SearchMobile>
+        </nav>
       </div>
     </header>
   );
