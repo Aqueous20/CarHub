04b7ec840a6eb826f8d03988ddef9d6f20d1f0c1
Corey Anderson <48457668+Aqueous20@users.noreply.github.com>
2024-03-21 15:26:39 -0500

    8th commit
    made a few changes trying to commit after every completed component but its a task to develop better programming habits im so used to committing the full project all at once

--- velocity/src/components/Search.js
+++ velocity/src/components/Search.js
@@ -1,11 +1,25 @@
-import React, { useContext } from 'react'
-import { SearchContext } from '../context/search'
+"use client";
+
+import React, { useContext } from "react";
+import { SearchContext } from "../context/search";
+import LocationSelector from "./LocationSelector";
 
 function Search() {
-  const {searchActive} = useContext(SearchContext)
+  const { searchActive } = useContext(SearchContext);
   return (
-    <div>Search</div>
-  )
+    <div
+      className={`${
+        searchActive
+          ? "bg-white rounded-none xl:h-[80px]"
+          : "bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]"
+      } hidden xl:block w-full relative shadow-lg`}
+    >
+      <div className="xl:h-full flex items-center px-6 xl:px-0">
+        <LocationSelector></LocationSelector>
+      </div>
+      
+    </div>
+  );
 }
 
-export default Search
+export default Search;
